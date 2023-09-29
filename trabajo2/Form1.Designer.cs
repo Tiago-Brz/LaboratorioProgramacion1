b09016b5c41d10e638ad8d35fb9244eacad05a84
Tiago <tiagobrizuela1@gmail.com>
2023-09-29 03:25:29 -0300

    tp2

---
+++ trabajo2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace trabajo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTirarDados = new System.Windows.Forms.Button();
+            this.lblResultados = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTirarDados
+            // 
+            this.btnTirarDados.BackColor = System.Drawing.Color.Red;
+            this.btnTirarDados.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnTirarDados.FlatAppearance.BorderSize = 2;
+            this.btnTirarDados.Location = new System.Drawing.Point(49, 85);
+            this.btnTirarDados.Name = "btnTirarDados";
+            this.btnTirarDados.Size = new System.Drawing.Size(107, 47);
+            this.btnTirarDados.TabIndex = 0;
+            this.btnTirarDados.Text = "Tirar Dados";
+            this.btnTirarDados.UseVisualStyleBackColor = false;
+            this.btnTirarDados.Click += new System.EventHandler(this.btnTirarDados_Click_1);
+            // 
+            // lblResultados
+            // 
+            this.lblResultados.AutoSize = true;
+            this.lblResultados.Location = new System.Drawing.Point(223, 85);
+            this.lblResultados.Name = "lblResultados";
+            this.lblResultados.Size = new System.Drawing.Size(0, 13);
+            this.lblResultados.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.Location = new System.Drawing.Point(379, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SALIR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(474, 228);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResultados);
+            this.Controls.Add(this.btnTirarDados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTirarDados;
+        private System.Windows.Forms.Label lblResultados;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ trabajo2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabajo2
+{
+    public partial class Form1 : Form
+    {
+        private int[][] resultados = new int[3][];
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTirarDados_Click_1(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < 3; i++)
+            {
+                resultados[i] = TirarDados();
+            }
+
+       
+            MostrarResultados();
+        }
+
+        private int[] TirarDados()
+        {
+            int[] dados = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                dados[i] = random.Next(1, 7); 
+            }
+
+            return dados;
+        }
+
+        private void MostrarResultados()
+        {
+            lblResultados.Text = "Resultados de las 3 tiradas:\n";
+
+            for (int i = 0; i < 3; i++)
+            {
+                lblResultados.Text += $"Tirada {i + 1}: {string.Join(", ", resultados[i])}\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
